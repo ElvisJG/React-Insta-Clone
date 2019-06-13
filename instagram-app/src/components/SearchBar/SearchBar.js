6caf34bde0d1717da4205310f536b92ccc9cb605
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-12 20:42:22 -0400

    the search bar has been slain

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from '../../instagramlogo.png';
 import './SearchBar.css';
+import { FaRegHeart } from 'react-icons/fa';
+import { FaRegCompass } from 'react-icons/fa';
+import { FaRegUser } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 
 export default () => {
   return (
@@ -9,11 +13,19 @@ export default () => {
         <img src={logo} alt='logo' className='logo' />
       </div>
       <div className='search-bar'>
-        <input type='text' placeholder='🔎 search' />
+        <input type='text' placeholder='🔍  search' />
       </div>
-      <div className='icons'>1</div>
-      <div className='icons'>2</div>
-      <div className='icons'>3</div>
+      <div className='icon-container'>
+        <div className='icons'>
+          <FaRegCompass size='1.5rem' />
+        </div>
+        <div className='icons'>
+          <FaRegHeart size='1.5rem' />
+        </div>
+        <div className='icons'>
+          <FaRegUser size='1.5rem' />
+        </div>
+      </div>
     </div>
   );
 };
